7d24ff21dfee1feb94563a2e186a019cbea42316
Cosmin-Romeo TANASE <tanasecosminromeo@gmail.com>
2021-02-13 14:55:49 +0200

    Update Taxon.php - Add slug

--- src/generated/Model/Taxon.php
+++ src/generated/Model/Taxon.php
@@ -19,6 +19,12 @@ class Taxon
     /**
      * 
      *
+     * @var string|null
+     */
+    protected $slug;
+    /**
+     * 
+     *
      * @var int|null
      */
     protected $position;
@@ -74,6 +80,27 @@ class Taxon
     public function setCode(?string $code) : self
     {
         $this->code = $code;
+        return $this;
+    }
+    /**
+     * 
+     *
+     * @return string|null
+     */
+    public function getSlug() : ?string
+    {
+        return $this->slug;
+    }
+    /**
+     * 
+     *
+     * @param string|null $slug
+     *
+     * @return self
+     */
+    public function setSlug(?string $slug) : self
+    {
+        $this->slug = $slug;
         return $this;
     }
     /**
